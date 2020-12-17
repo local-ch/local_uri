c63ec132b80f4140e8f4f2b302800aa6a0200d8c
Sebastian Pape <s@pape.io>
2020-12-17 11:47:04 +0100

    fix rubocop

--- spec/query/except_spec.rb
+++ spec/query/except_spec.rb
@@ -8,14 +8,14 @@ describe LocalUri::URI do
 
     it 'Returns a uri which query includes everything except given keys' do
       uri = subject.query.except(:two, :three)
-      expect(uri).to be_kind_of(LocalUri::URI)
+      expect(uri).to be_kind_of(described_class)
       expect(uri.to_s).to eq 'https://local.ch?one=1'
       expect(subject.to_s).to eq 'https://local.ch?one=1&two=2&three=3'
     end
 
     it 'Removes the given keys from the query of the original uri and returns the uri itself' do
-      _return = subject.query.except!(:two, :three)
-      expect(_return).to be_kind_of(LocalUri::URI)
+      duplicate = subject.query.except!(:two, :three)
+      expect(duplicate).to be_kind_of(described_class)
       expect(subject.to_s).to eq 'https://local.ch?one=1'
     end
   end
